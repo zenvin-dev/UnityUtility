e903f5a3c7b407ef490cc6ed9dd402a1fb954c26
Zenvin <zenvin@outlook.de>
2023-09-28 18:27:09 +0200

    - Move `StateQueueChangedArgs` to its own file

---
+++ Runtime/StateQueue/StateQueueChangedArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace Zenvin.Utility {
+	public class StateQueueChangedArgs<T> {
+		public readonly IStateQueue<T> Origin;
+		public readonly T Previous;
+		public readonly T Current;
+
+
+		private StateQueueChangedArgs () { }
+
+		internal StateQueueChangedArgs (IStateQueue<T> origin, T previous, T current) {
+			Origin = origin;
+			Previous = previous;
+			Current = current;
+		}
+	}
+}
