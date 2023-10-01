53c1a25f0d62f8588217adcbd5c49f89403607ef
Zenvin <zenvin@outlook.de>
2023-10-01 11:00:35 +0200

    - Add documentation to `StateQueueChangedArgs`

--- Runtime/StateQueue/StateQueueChangedArgs.cs
+++ Runtime/StateQueue/StateQueueChangedArgs.cs
@@ -1,7 +1,16 @@
 ﻿namespace Zenvin.Util {
 	public class StateQueueChangedArgs<T> {
+		/// <summary>
+		/// The queue that triggered the event.
+		/// </summary>
 		public readonly IStateQueue<T> Origin;
+		/// <summary>
+		/// The value that the queue had prior to the change that triggered the event.
+		/// </summary>
 		public readonly T Previous;
+		/// <summary>
+		/// The value that the queue has now.
+		/// </summary>
 		public readonly T Current;
 
 
