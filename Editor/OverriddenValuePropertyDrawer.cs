edbab07449b665f1598e495051d7306a2ae7f667
Zenvin <zenvin@outlook.de>
2023-12-10 12:45:47 +0100

    - Update `OverriddenValuePropertyDrawer`
    - Enable previewing overridden value

--- Editor/OverriddenValuePropertyDrawer.cs
+++ Editor/OverriddenValuePropertyDrawer.cs
@@ -8,21 +8,83 @@ namespace Zenvin.Util.Presets {
 
 		private static readonly Rect[] parts = new Rect[2];
 
+		private static GUIContent showContent;
+		private static GUIContent ShowContent {
+			get {
+				if (showContent == null) {
+					showContent = EditorGUIUtility.IconContent ("d_animationvisibilitytoggleon");
+					showContent.tooltip = "Pre-viewing preset value.";
+				}
+				return showContent;
+			}
+		}
+		private static GUIContent hideContent;
+		private static GUIContent HideContent {
+			get {
+				if (hideContent == null) {
+					hideContent = EditorGUIUtility.IconContent ("d_animationvisibilitytoggleoff");
+					hideContent.tooltip = "Pre-viewing default value.";
+				}
+				return hideContent;
+			}
+		}
+
+		private SerializedObject presetObject = null;
+		private bool preview = true;
+
 
 		public override void OnGUI (Rect position, SerializedProperty property, GUIContent label) {
 			label = EditorGUI.BeginProperty (position, label, property);
 
 			var valueProp = property.FindPropertyRelative ("value");
 			var presetProp = property.FindPropertyRelative ("preset");
+			UpdatePreset (presetProp.objectReferenceValue);
+
+			var hasPreset = presetProp.objectReferenceValue != null;
+			var presetValueProp = presetObject?.FindProperty ("value");
 
 			position = EditorGUI.PrefixLabel (position, label);
 			position.SplitNonAlloc (RectTransform.Axis.Horizontal, parts, EditorGUIUtility.standardVerticalSpacing);
 
-			EditorGUI.PropertyField (parts[0], valueProp, GUIContent.none);
-			EditorGUI.PropertyField (parts[1], presetProp, GUIContent.none);
+			DrawValueProperty (valueProp, parts[0], hasPreset, presetValueProp);
+			DrawPresetProperty (presetProp, parts[1], hasPreset);
 
 			property.serializedObject.ApplyModifiedProperties ();
 			EditorGUI.EndProperty ();
+		}
+
+		private void DrawValueProperty (SerializedProperty prop, Rect position, bool hasPreset, SerializedProperty presetValueProp) {
+			if (hasPreset && preview) {
+				GUI.enabled = false;
+				EditorGUI.PropertyField (position, presetValueProp, GUIContent.none);
+				GUI.enabled = true;
+				return;
+			}
+			EditorGUI.PropertyField (position, prop, GUIContent.none);
+		}
+
+		private void DrawPresetProperty (SerializedProperty prop, Rect position, bool hasPreset) {
+			if (hasPreset) {
+				var spacing = EditorGUIUtility.standardVerticalSpacing;
+				position = position.Inset (RectTransform.Edge.Right, EditorGUIUtility.singleLineHeight + spacing, out var inset);
+				inset.x += spacing;
+				inset.width -= spacing;
+				if (GUI.Button(inset, preview ? ShowContent : HideContent, EditorStyles.label)) {
+					preview ^= true;
+				}
+			}
+			EditorGUI.PropertyField (position, prop, GUIContent.none);
+		}
+
+		private void UpdatePreset (Object target) {
+			if (target == null) {
+				presetObject = null;
+				return;
+			}
+			if (presetObject != null && presetObject.targetObject == target) {
+				return;
+			}
+			presetObject = new SerializedObject (target);
 		}
 	}
 }
